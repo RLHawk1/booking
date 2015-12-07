0047b1ade78543819bcccdace74e872ffcd99c7a
KonstantinVlasov <konstantinnvlasov@gmail.com>
2015-12-08 00:51:06 +0300

    quote post endpoint

--- src/net/cbtltd/rest/quote/QuoteRest.java
+++ src/net/cbtltd/rest/quote/QuoteRest.java
@@ -1,6 +1,7 @@
 package net.cbtltd.rest.quote;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -9,28 +10,30 @@ import com.bookingnet.service.MailService;
 import net.cbtltd.shared.Mail;
 
 import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * The Class FlipkeyRest.
  */
 @Path("/quote")
-@Produces("application/xml")
+@Produces("application/json")
 public class QuoteRest {
 
 	private static final Logger LOG = Logger.getLogger(QuoteRest.class.getName());
 
-	@GET
+	@POST
 	@Path("/send")
-	public static synchronized String send() {
+	public static synchronized String send(@RequestBody String json) {
 
 		Mail mail = new Mail();
 		mail.setSubject("Quote send endpoint");
-		mail.setContent("Testing email");
+		mail.setContent(json);
 		mail.setRecipients("mike793@gmail.com, konstantinnvlasov@gmail.com");
 		MailService.send(mail);
-		
+
+		System.out.println(json);
 		System.out.println("test email sending");
 		
-		return "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><sent/>";
+		return "{\"success\": true}";
 	}
 }
